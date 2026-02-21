968f6a991fb2295d870d2b227aa4f438c5d31d88
Ike Iloegbu <Ike@i2Mastering.com>
2026-02-21 08:50:41 -0500

    Initial Commit

---
+++ Konekt AudioEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Konekt AudioEngine
+//
+//  Created by ike iloegbu on 2/21/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
